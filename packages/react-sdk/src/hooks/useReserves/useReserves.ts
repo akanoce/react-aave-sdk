c9f488377c7f05f0c3184499933cc4658cbed8de
Erik Nucibella <eriknucib@live.it>
2024-04-04 22:43:50 +0200

    feat(sdk/hooks): add react-query based useReserves to fetch userReserves on any network

---
+++ packages/react-sdk/src/hooks/useReserves/useReserves.ts
@@ -0,0 +1,66 @@
+import {
+  ReserveDataHumanized,
+  ReservesDataHumanized,
+  UiPoolDataProvider,
+} from "@aave/contract-helpers";
+import { FormatReserveUSDResponse, formatReserves } from "@aave/math-utils";
+import { AaveV3Sepolia } from "@bgd-labs/aave-address-book";
+import { useQuery } from "@tanstack/react-query";
+import dayjs from "dayjs";
+import { useAaveContracts } from "../../providers/AaveContractsProvider";
+
+type GetReservesResponse = {
+  reserves: ReservesDataHumanized;
+  formattedReserves: (ReserveDataHumanized & FormatReserveUSDResponse)[];
+};
+/**
+ * Fetches pool reserves and market base currency data from the Aave V3 UI Pool Data Provider contract
+ * @param poolDataProviderContract   The Aave V3 UI Pool Data Provider contract
+ * @param chainAddressBook  The Aave V3 address book for the current chain
+ * @returns  Object containing array of pool reserves and market base currency data
+ */
+export const getReserves = async (
+  poolDataProviderContract: UiPoolDataProvider,
+  chainAddressBook: typeof AaveV3Sepolia
+): Promise<GetReservesResponse> => {
+  // Object containing array of pool reserves and market base currency data
+  // { reservesArray, baseCurrencyData }
+  const reserves = await poolDataProviderContract.getReservesHumanized({
+    lendingPoolAddressProvider: chainAddressBook.POOL_ADDRESSES_PROVIDER,
+  });
+  return {
+    reserves,
+    formattedReserves: formatReserves({
+      reserves: reserves.reservesData,
+      currentTimestamp: dayjs().unix(),
+      marketReferenceCurrencyDecimals:
+        reserves.baseCurrencyData.marketReferenceCurrencyDecimals,
+      marketReferencePriceInUsd:
+        reserves.baseCurrencyData.marketReferenceCurrencyPriceInUsd,
+    }),
+  };
+};
+
+export const getReservesQueryKey = (chainId: string | number) => [
+  "AAVE",
+  chainId,
+  "RESERVES",
+];
+
+/**
+ *  Fetches pool reserves and market base currency data from the Aave V3 UI Pool Data Provider contract
+ * @returns  Query object containing array of pool reserves and market base currency data
+ */
+export const useReserves = () => {
+  const { poolDataProviderContract, chainAddressBook } = useAaveContracts();
+
+  const enabled = !!poolDataProviderContract && !!chainAddressBook;
+  return useQuery({
+    queryKey: getReservesQueryKey(chainAddressBook.CHAIN_ID),
+    queryFn: async () =>
+      enabled
+        ? await getReserves(poolDataProviderContract, chainAddressBook)
+        : null,
+    enabled,
+  });
+};
