c54f353f2cecb61483da8f6f3c34ca34df28d15e
Erik Nucibella <eriknucib@live.it>
2024-05-04 00:42:32 +0200

    feat(sdk/AaveContractsProvider): support for almost every addressBook excluding metis

--- apps/vite-app/src/App.tsx
+++ apps/vite-app/src/App.tsx
@@ -2,11 +2,12 @@ import { VStack } from "@chakra-ui/react";
 import { UserReserves } from "./components";
 import { Reserves } from "./components/Reserves";
 import { useEthersProvider } from "./hooks/useEthersProvider";
-import { mainnet, sepolia } from "viem/chains";
+import { sepolia } from "viem/chains";
 import { AaveContractsProvider } from "@aave/react-sdk";
 
 export const App = () => {
   const provider = useEthersProvider({ chainId: sepolia.id });
+  console.log("p", provider);
   return (
     <AaveContractsProvider provider={provider}>
       <VStack spacing={4} w="full">
--- packages/react-sdk/src/providers/AaveContractsProvider.tsx
+++ packages/react-sdk/src/providers/AaveContractsProvider.tsx
@@ -3,14 +3,75 @@ import {
   UiIncentiveDataProvider,
   UiPoolDataProvider,
 } from "@aave/contract-helpers";
-import { AaveV3Sepolia } from "@bgd-labs/aave-address-book";
+import {
+  AaveV3Arbitrum,
+  AaveV3ArbitrumSepolia,
+  AaveV3Avalanche,
+  AaveV3Base,
+  AaveV3BaseSepolia,
+  AaveV3Ethereum,
+  AaveV3Fantom,
+  AaveV3FantomTestnet,
+  AaveV3Fuji,
+  AaveV3Gnosis,
+  AaveV3Harmony,
+  AaveV3Mumbai,
+  AaveV3Optimism,
+  AaveV3OptimismSepolia,
+  AaveV3Polygon,
+  AaveV3PolygonZkEvm,
+  AaveV3Scroll,
+  AaveV3ScrollSepolia,
+  AaveV3Sepolia,
+} from "@bgd-labs/aave-address-book";
 import { createContext, useContext, useMemo } from "react";
 import { providers } from "ethers";
+import {
+  arbitrum,
+  arbitrumSepolia,
+  avalanche,
+  avalancheFuji,
+  base,
+  baseSepolia,
+  fantom,
+  fantomTestnet,
+  gnosis,
+  harmonyOne,
+  mainnet,
+  sepolia,
+  polygonMumbai,
+  polygon,
+  polygonZkEvm,
+  optimism,
+  optimismSepolia,
+  scroll,
+  scrollSepolia,
+} from "viem/chains";
 
 interface CurrentUserContextType {
   poolDataProviderContract: UiPoolDataProvider | null;
   incentiveDataProviderContract: UiIncentiveDataProvider | null;
-  chainAddressBook: typeof AaveV3Sepolia;
+  chainAddressBook:
+    | typeof AaveV3Sepolia
+    | typeof AaveV3Ethereum
+    | typeof AaveV3ScrollSepolia
+    | typeof AaveV3Arbitrum
+    | typeof AaveV3ArbitrumSepolia
+    | typeof AaveV3Avalanche
+    | typeof AaveV3Base
+    | typeof AaveV3BaseSepolia
+    | typeof AaveV3Fantom
+    | typeof AaveV3FantomTestnet
+    | typeof AaveV3Gnosis
+    | typeof AaveV3Harmony
+    | typeof AaveV3Fuji
+    | typeof AaveV3Mumbai
+    | typeof AaveV3Polygon
+    | typeof AaveV3PolygonZkEvm
+    | typeof AaveV3Optimism
+    | typeof AaveV3OptimismSepolia
+    | typeof AaveV3Scroll
+    | typeof AaveV3ScrollSepolia;
   poolContract: Pool | null;
 }
 
@@ -19,9 +80,60 @@ const AaveContractsContext = createContext<CurrentUserContextType | null>(null);
 type Props = {
   children: React.ReactNode;
   provider: providers.Provider;
+  chainId: number;
 };
-export const AaveContractsProvider = ({ children, provider }: Props) => {
-  const chainAddressBook = useMemo(() => AaveV3Sepolia, []);
+export const AaveContractsProvider = ({
+  children,
+  provider,
+  chainId,
+}: Props) => {
+  const chainAddressBook = useMemo(() => {
+    switch (chainId) {
+      //TODO: BNB ?
+      case sepolia.id:
+        return AaveV3Sepolia;
+      case mainnet.id:
+        return AaveV3Ethereum;
+      case arbitrum.id:
+        return AaveV3Arbitrum;
+      case arbitrumSepolia.id:
+        return AaveV3ArbitrumSepolia;
+      case avalanche.id:
+        return AaveV3Avalanche;
+      case base.id:
+        return AaveV3Base;
+      case baseSepolia.id:
+        return AaveV3BaseSepolia;
+      case fantom.id:
+        return AaveV3Fantom;
+      case fantomTestnet.id:
+        return AaveV3FantomTestnet;
+      case gnosis.id:
+        return AaveV3Gnosis;
+      case harmonyOne.id:
+        return AaveV3Harmony;
+      case avalancheFuji.id:
+        return AaveV3Fuji;
+      //   case metis.id:
+      //     return AaveV3Metis; // do not support WETH_GATEWAY
+      case polygonMumbai.id:
+        return AaveV3Mumbai;
+      case polygon.id:
+        return AaveV3Polygon;
+      case polygonZkEvm.id:
+        return AaveV3PolygonZkEvm;
+      case optimism.id:
+        return AaveV3Optimism;
+      case optimismSepolia.id:
+        return AaveV3OptimismSepolia;
+      case scroll.id:
+        return AaveV3Scroll;
+      case scrollSepolia.id:
+        return AaveV3ScrollSepolia;
+      default:
+        throw new Error("Chain not supported");
+    }
+  }, [chainId]);
 
   // View contract used to fetch all reserves data (including market base currency data), and user reserves
   // Using Aave V3 Eth Mainnet address for demo
