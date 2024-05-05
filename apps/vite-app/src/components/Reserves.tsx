1076724fdf07a8baa9ff63661885092fc1c7f50d
Erik Nucibella <eriknucib@live.it>
2024-05-05 14:35:33 +0200

    feat(vite-app): polish loading, add totalDebt and totalLiquidity

--- apps/vite-app/src/components/Reserves.tsx
+++ apps/vite-app/src/components/Reserves.tsx
@@ -1,29 +1,82 @@
-import { useReserves } from "@aave/react-sdk";
-import { Card, CardHeader, Heading, Spinner } from "@chakra-ui/react";
+import { formatBalance, useReserves } from "@aave/react-sdk";
+import {
+  Box,
+  Card,
+  CardBody,
+  CardHeader,
+  HStack,
+  Heading,
+  Spinner,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { ReservesTable } from "@repo/components";
+import { useMemo } from "react";
 
 export const Reserves = () => {
   const { data: reserves, isLoading: reservesLoading } = useReserves();
 
-  if (reservesLoading) return;
-  <Card w="full">
-    <CardHeader>
-      <Heading size="md">Aave V3 Reserves</Heading>
-    </CardHeader>
-    <Spinner />
-  </Card>;
+  const totalLiquidity = useMemo(
+    () =>
+      reserves?.formattedReserves.reduce(
+        (acc, reserve) => acc + Number(reserve.totalLiquidityUSD),
+        0
+      ),
+    [reserves]
+  );
+
+  const totalDebt = useMemo(
+    () =>
+      reserves?.formattedReserves.reduce(
+        (acc, reserve) => acc + Number(reserve.totalDebtUSD),
+        0
+      ),
+    [reserves]
+  );
+
+  if (reservesLoading)
+    return (
+      <Card w="full">
+        <CardHeader>
+          <Heading size="md">Aave V3 Reserves</Heading>
+        </CardHeader>
+        <CardBody>
+          <Spinner />
+        </CardBody>
+      </Card>
+    );
 
   if (!reserves) return null;
 
   return (
     <Card w="full">
       <CardHeader>
-        <Heading size="md">Aave V3 Reserves</Heading>
+        <VStack align="start" spacing={2} w="full">
+          <Heading size="md">Aave V3 Reserves</Heading>
+          <HStack spacing={4}>
+            <Box>
+              <Text textTransform={"uppercase"} color="gray.500">
+                Total Liquidity
+              </Text>
+              <Heading size="sm">
+                {formatBalance(totalLiquidity, "USD")}
+              </Heading>
+            </Box>
+            <Box>
+              <Text textTransform={"uppercase"} color="gray.500">
+                Total debt
+              </Text>
+              <Heading size="sm">{formatBalance(totalDebt, "USD")}</Heading>
+            </Box>
+          </HStack>
+        </VStack>
       </CardHeader>
-      <ReservesTable
-        formattedReserves={reserves.formattedReserves}
-        tableCaption="Aave V3 Reserves"
-      />
+      <CardBody>
+        <ReservesTable
+          formattedReserves={reserves.formattedReserves}
+          tableCaption="Aave V3 Reserves"
+        />
+      </CardBody>
     </Card>
   );
 };
