8dacfcdc7ff8b654ace256a4c944643f194e8772
Erik Nucibella <eriknucib@live.it>
2024-05-07 23:43:01 +0200

    fix(eslint): add tsconfigRootDir: __dirname to have eslint correctly resolve tsconfig files

--- packages/react-sdk/.eslintrc.cjs
+++ packages/react-sdk/.eslintrc.cjs
@@ -2,24 +2,25 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'airbnb',
-    'airbnb-typescript',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'prettier'
+    "eslint:recommended",
+    "airbnb",
+    "airbnb-typescript",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: './tsconfig.json'
+    project: "./tsconfig.json",
   },
-  plugins: ['prettier','react-refresh'],
+  plugins: ["prettier", "react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
-    'prettier/prettier': ["error"]
+    "prettier/prettier": ["error"],
   },
-}
+  tsconfigRootDir: __dirname,
+};
