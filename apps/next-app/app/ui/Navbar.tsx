8884db13e34fe753a653e593267a84d670c5a583
Erik Nucibella <eriknucib@live.it>
2024-05-08 18:47:23 +0200

    feat(components): add and integrate theme-swticher

--- apps/next-app/app/ui/Navbar.tsx
+++ apps/next-app/app/ui/Navbar.tsx
@@ -1,10 +1,12 @@
 import { Heading, HStack } from "@chakra-ui/react";
 import { NetworkSelect } from "@/app/ui/NetworkSelect";
+import { ThemeSwitcher } from "@repo/components";
 
 export const Navbar = () => (
   <HStack as="nav" bg="gray.800" color="white" p={4} justify="space-between">
     <Heading size="md">Vite App</Heading>
-    <HStack>
+    <HStack spacing={2}>
+      <ThemeSwitcher />
       <NetworkSelect />
     </HStack>
   </HStack>
--- apps/vite-app/src/components/Navbar.tsx
+++ apps/vite-app/src/components/Navbar.tsx
@@ -1,11 +1,14 @@
 import { Heading, HStack } from "@chakra-ui/react";
 import { NetworkSelect } from "./NetworkSelect";
+import { ThemeSwitcher } from "@repo/components";
+
 
 export const Navbar = () => {
   return (
     <HStack as="nav" bg="gray.800" color="white" p={4} justify="space-between">
       <Heading size="md">Vite App</Heading>
-      <HStack>
+      <HStack spacing={2}>
+        <ThemeSwitcher />
         <NetworkSelect />
       </HStack>
     </HStack>
