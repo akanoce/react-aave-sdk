1e0c3ab2887af0554d9d59f1647e21c40c87c62b
gystemd <gystemd@gmail.com>
2024-05-15 17:49:24 +0200

    feat(user-reserve): add user reserves table

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./ReservesTable/ReservesTable";
+export * from "./UserReservesTable/UserReservesTable";
 export * from "./ThemeSwitcher/ThemeSwitcher";
 export * from "./CryptoIcons";
